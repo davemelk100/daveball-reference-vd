830ed612ed20126f087afbf5211f9c7631dde82d
davemelk100 <davemelk@gmail.com>
2026-01-14 15:08:18 -0500

    bigger rando

--- components/player-spotlight.tsx
+++ components/player-spotlight.tsx
@@ -40,7 +40,7 @@ export function PlayerSpotlight() {
               alt={player.name}
               width={213}
               height={213}
-              className="w-auto h-[80px] sm:h-[200px] rounded-xl transition-transform group-hover:scale-105"
+              className="w-auto rounded-xl transition-transform group-hover:scale-105"
               onError={() => setImageError(true)}
               priority
             />
