379b96fd962c08f61f938a385834461c1b8e7ebf
davemelk100 <davemelk@gmail.com>
2026-01-27 13:02:07 -0500

    lots

--- lib/amrep-member-images.ts
+++ lib/amrep-member-images.ts
@@ -63,7 +63,7 @@ export const AMREP_MEMBER_IMAGE_FALLBACKS: Record<string, string> = {
   "the thrown ups":
     "/api/gbv/image-proxy?url=https%3A%2F%2Flastfm.freetls.fastly.net%2Fi%2Fu%2Far0%2Fece7b846cab5c525fc0eb7147fb86ed0.jpg",
   vertigo:
-    "/api/gbv/image-proxy?url=https%3A%2F%2Fblogger.googleusercontent.com%2Fimg%2Fb%2FR29vZ2xl%2FAVvXsEipd6KiHDAbQRs8CrRmxCtqbg5yoSbtwAR2w4FDFERXJfP-Wgf3Cz4jKCnaAK371bePkk6lKYG-xjoKhUdF7yW1blhH6YjV-KoEofpS4GD9XwZyDKQbxxbHb3Q5NFTmvea6ecFdMdVZWqoj%2Fs854-r%2F1.jpg",
+    "/api/gbv/image-proxy?url=https%3A%2F%2Fi.discogs.com%2FjTmauyEW5UcThw_RdMzp2edmusqGm1O-45bkFQG2d18%2Frs%3Afit%2Fg%3Asm%2Fq%3A90%2Fh%3A462%2Fw%3A600%2FczM6Ly9kaXNjb2dz%2FLWRhdGFiYXNlLWlt%2FYWdlcy9BLTI2NTQw%2FNS0xMzM1ODg1MDMw%2FLmpwZWc.jpeg",
 };
 
 export const AMREP_MEMBER_IMAGE_SKIP: Record<string, true> = {
